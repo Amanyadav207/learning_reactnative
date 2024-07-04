60a9b0bb86ac67d508f9498f7c75144740bd1811
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2024-07-04 13:00:39 +0530

    learned how to create multiple screens

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World !! </Text>
       <StatusBar style="auto" />
+    <Link href="./profile" style ={{color :'blue'}} >Profile</Link>
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
